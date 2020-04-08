f355da6a6c72045fd40d420a063ef0c137a982cf
siddhesh-sovitkar <58511972+siddhesh-sovitkar@users.noreply.github.com>
2020-04-08 18:21:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import AlertTemplate from "react-alert-template-basic";
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AlertProvider template={AlertTemplate}>
+      <App />
+    </AlertProvider>
   </React.StrictMode>,
   rootElement
 );
